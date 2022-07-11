9e0ec08627bbaa5d7b27e37ed7b6856e87c05571
Ruan Berté <berte.ruan@gmail.com>
2022-07-11 17:14:39 -0300

    feat: install and configure eslint and prettier with husky

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  quoteProps: 'preserve',
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxBracketSameLine: true,
+  printWidth: 80,
+}
